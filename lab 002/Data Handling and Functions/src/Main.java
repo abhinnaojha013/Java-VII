1209e0f7bea73c5dae0af6ff3f5b3b3f76890d37
Abhinna Ojha <abhinnacsit18@oic.edu.np>
2022-12-06 15:14:08 +0545

    Lab 002 new

--- lab 002/Data Handling and Functions/src/Main.java
+++ lab 002/Data Handling and Functions/src/Main.java
@@ -1,24 +1,54 @@
-import java.util.Scanner;
+/*
+ *  Title:
+ *  1.  Write a program to accept 5 employee IDs and the corresponding names and their salaries
+ *      from the user and store them in three arrays. Pass these arrays to a function display() as
+ *      arguments. This display() will display the content of the arrays in the following format.
+ *
+ *  2.  Write another function display() with Employee ID array and Employee name array as
+ *      arguments.
+ *
+ *  3.  Write another function named display() which takes 4 arguments. The arguments are named
+ *      as String and 3 arrays (Employee id, name and salary). Function prototype looks like:
+ *      display (String name, int regno[], String Empname[], double salary[]).
+ *
+ *
+ *
+ *  Date modified; Author(s); Modification details
+ *      2022-12-06; abhinna; Created the program
+ * */
 
+import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args)
     {
-        Employee q1 = new Employee();
+//        declaring the arrays
+        int[] regno = new int[5];
+        String[] empName = new String[5];
+        double[] salary = new double[5];
+
+//        accepting data for arrays
+        inputData(regno, empName, salary);
+
         System.out.println("");
 
-        q1.display(q1.id, q1.name, q1.salary);
+//        Q1 output
+        display(regno, empName, salary);
+//        Q2 output
+        display(regno, empName);
+
+//        accept name to search
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\n\n\nEnter the name to search: ");
+        String name = scanner.nextLine();
+
+//        Q3 output
+        display(name, regno, empName, salary);
+
     }
-}
 
-class Employee
-{
-    int[] id = new int[5];
-    String[] name = new String[5];
-    double[] salary = new double[5];
-
-    //    constructor to get input from the user
-    Employee()
+//    method to get input from the user
+    public static void inputData(int[] regno, String[] empName, double[] salary)
     {
         Scanner scanner = new Scanner(System.in);
 
@@ -26,36 +56,76 @@ class Employee
         {
 //            nextLine() for String and nextInt() for Integer
             System.out.print("\nEnter employee id: ");
-            id[i] = scanner.nextInt();
+            regno[i] = scanner.nextInt();
 
 //            using a dummy nextLine() as nextLine() takes previous line as input after nextInt()
-            name[i] = scanner.nextLine();
+            empName[i] = scanner.nextLine();
 
             System.out.print("Enter employee name: ");
-            name[i] = scanner.nextLine();
+            empName[i] = scanner.nextLine();
 
             System.out.print("Enter employee salary: ");
             salary[i] = scanner.nextDouble();
         }
     }
-
-    public void display(int[] id, String[] name, double[] salary)
+    public static void display(int[] regno, String[] empName, double[] salary)
     {
+        System.out.println("\n\n3 argument display");
         System.out.println("ID\t\tName\t\t\tSalary");
         for (int i = 0; i < 5; i++)
         {
 //            using if to adhere to the 3 digit format of id
-            if (id[i] < 10)
+            if (regno[i] < 10)
             {
                 System.out.print("00");
             }
-            else if (id[i] < 100)
+            else if (regno[i] < 100)
             {
                 System.out.print("0");
             }
-            System.out.println(id[i] + "\t\t" + name[i] + "\t\t\t" + salary[i] );
+            System.out.println(regno[i] + "\t\t" + empName[i] + "\t\t\t" + salary[i] );
         }
     }
 
+//    overloading display(int[], String[], double[])
+    public static void display(int[] regno, String[] empName)
+    {
+        System.out.println("\n\n2 argument display");
+        System.out.println("ID\t\tName");
+        for (int i = 0; i < 5; i++)
+        {
+//            using if to adhere to the 3 digit format of id
+            if (regno[i] < 10)
+            {
+                System.out.print("00");
+            }
+            else if (regno[i] < 100)
+            {
+                System.out.print("0");
+            }
+            System.out.println(regno[i] + "\t\t" + empName[i]);
+        }
+    }
 
+//    overloading display() to search and display
+    public static void display (String name, int[] regno, String[] empName, double[] salary)
+    {
+        System.out.println("\n\n4 argument display");
+        System.out.println("ID\t\tName\t\t\tSalary");
+        for (int i = 0; i < 5; i++)
+        {
+            if (name.equalsIgnoreCase(empName[i]))
+            {
+                if (regno[i] < 10)
+                {
+                    System.out.print("00");
+                }
+                else if (regno[i] < 100)
+                {
+                    System.out.print("0");
+                }
+                System.out.println(regno[i] + "\t\t" + empName[i] + "\t\t\t" + salary[i] );
+            }
+        }
+    }
 }
