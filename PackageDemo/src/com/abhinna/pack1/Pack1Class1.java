af842696788d30a97a00cfd58eefc3859223d4fa
Abhinna Ojha <abhinnacsit18@gmail.com>
2022-12-23 08:10:45 +0545

    Upload PackageDemo

---
+++ PackageDemo/src/com/abhinna/pack1/Pack1Class1.java
@@ -0,0 +1,9 @@
+package com.abhinna.pack1;
+
+public class Pack1Class1
+{
+    public double area(double r)
+    {
+        return r * r;
+    }
+}
---
+++ PackageDemo/src/com/abhinna/pack2/Pack2Class1.java
@@ -0,0 +1,12 @@
+package com.abhinna.pack2;
+
+import com.abhinna.pack1.Pack1Class1;
+
+public class Pack2Class1
+{
+    public static void main(String[] args)
+    {
+        Pack1Class1 pack1Class1 = new Pack1Class1();
+        System.out.println(pack1Class1.area(4.5));
+    }
+}
